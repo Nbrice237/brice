39d9e79daad3fad26f3c853012df819a40092112
Nbrice237 <makebelbrice@gmail.com>
2023-01-30 16:22:20 +0100

    Initial commit

---
+++ Calculs/Program.cs
@@ -0,0 +1,109 @@
+﻿/**
+ * Application "Calculs" v01 (b5 a218)
+ * author : Emds
+ * date : 24/05/2020
+ */
+using System;
+
+namespace Calculs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables 
+            Random rand = new Random(); // outil de génération de nombre aléatoire
+            int val1, val2; // mémorisation de nombres aléatoires
+            int solution; // calcul de la solution
+            int reponse = 0; // saisie de la réponse de l'utilisateur
+            string choix; // saisie du choix de l'utilsiateur
+            bool correct; // mémorise si la saisie est un entier
+
+            // boucle sur le menu
+            choix = "1";
+            while (choix != "0")
+            {
+                // affiche le menu et saisi le choix
+                Console.WriteLine("Addition ....................... 1");
+                Console.WriteLine("Multiplication ................. 2");
+                Console.WriteLine("Quitter ........................ 0");
+                Console.Write("Choix :                          ");
+                choix = Console.ReadLine();
+
+                // traitement des choix
+                switch (choix)
+                {
+                    case "0":// fin du programme
+                        break;
+
+                    case "1":// addition
+                        // choix de l'addition
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
+                        correct = false;
+
+                        while (!correct)
+                        {
+                            try
+                            {
+                                // saisie de la réponse
+                                Console.Write(val1 + " + " + val2 + " = ");
+                                reponse = int.Parse(Console.ReadLine());
+                                correct = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Saisissez un entier");
+                            }
+                        }
+                        // comparaison avec la bonne réponse
+                        solution = val1 + val2;
+                        if (reponse == solution)
+                        {
+                            Console.WriteLine("Bravo !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Faux : " + val1 + " + " + val2 + " = " + solution);
+                        }
+                        break;
+
+                    case "2": // multiplication
+                        // choix de la multiplication
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
+                        correct = false;
+                        while (!correct)
+                        {
+                            try
+                            {
+                                // saisie de la réponse
+                                Console.Write(val1 + " x " + val2 + " = ");
+                                reponse = int.Parse(Console.ReadLine());
+                                correct = true;
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Saisissez un entier");
+                            }
+                        }
+                        // comparaison avec la bonne réponse
+                        solution = val1 * val2;
+                        if (reponse == solution)
+                        {
+                            Console.WriteLine("Bravo !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Faux : " + val1 + " x " + val2 + " = " + solution);
+                        }
+                        break;
+
+                    default: // autre valeur donc erreur de saisie
+                        Console.WriteLine("Erreur de saisie");
+                        break;
+                }
+            }
+        }
+    }
+}
